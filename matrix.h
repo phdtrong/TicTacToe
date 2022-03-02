58fa8f40a9c1a3ac07593d68132261fa482f479f
phdtrong <phdtrong200748@gmail.com>
2022-03-02 19:08:25 +0000

    Update matrix.h header's comment and private properties

--- matrix.h
+++ matrix.h
@@ -3,13 +3,13 @@ CSUF, CPSC254 - SPRING 2022
 Contributor: Trong Pham, Adam Harb, Albert Paez
 Project Title: Tictactoe Game
 Project Description: Standalone game allows user to beat AI.
-										 The winner is defined that has a cross
-										 of row, column, or diagonal. This game
-										 is designed with a smart AI to challenge
-										 players like you!
-										 This project includes main files of matrix
-										 operators, tictactoe game driver, and main
-										 file to call driver and load the game.
+  The winner is defined that has a cross
+  of row, column, or diagonal. This game
+  is designed with a smart AI to challenge
+  players like you!
+  This project includes main files of matrix
+  operators, tictactoe game driver, and main
+  file to call driver and load the game.
 License: Included but not limited of MIT, Harvard, CSUF, Github
 File Name: matrix.h
 File Description: Includes all matrix operator declartions.
@@ -21,15 +21,16 @@ File Description: Includes all matrix operator declartions.
 
 template <class T> class matrix
 {
-	//TODO
+	private:
+		T mat[SIDE][SIDE];
 };
 #include "matrix.cpp"
 #endif
 /*===============================================================
 Side node: This is the link to resolve linking issues that is
-						associated with the separation of .h and .cpp files.
+  associated with the separation of .h and .cpp files.
 Link: https://tinyurl.com/Hehehe121
 Selected solution: add .cpp file at the end of .h file as includers
 Original link:www.codeproject.com/Articles/48575/
-							How-to-Define-a-Template-Class-in-a-h-File-and-Imp
+How-to-Define-a-Template-Class-in-a-h-File-and-Imp
 ================================================================*/
