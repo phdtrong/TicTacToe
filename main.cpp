763db93bcee343d4c8cf910efcceee644fd2b2f1
phdtrong <70780829+phdtrong@users.noreply.github.com>
2022-03-07 08:00:28 -0800

    Update main.cpp
    Update the header comment

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@ CSUF, CPSC254 - SPRING 2022
 Contributor: Trong Pham, Adam Harb, Albert Paez
 Project Title: Tictactoe Game
 Project Description: Standalone game allows user to beat AI.
-										 The winner is defined that has a cross
-										 of row, column, or diagonal. This game
-										 is designed with a smart AI to challenge
-										 players like you!
-										 This project includes main files of matrix
-										 operators, tictactoe game driver, and main
-										 file to call driver and load the game.
+  The winner is defined that has a cross
+  of row, column, or diagonal. This game
+  is designed with a smart AI to challenge
+  players like you!
+  This project includes main files of matrix
+  operators, tictactoe game driver, and main
+  file to call driver and load the game.
 License: Included but not limited of MIT, Harvard, CSUF, Github
 File Name: main.cpp
 File Description: Calling game driver file to load the game.
