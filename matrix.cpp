5a274fb659b896a6cde7d6365868b4f468497e80
phdtrong <70780829+phdtrong@users.noreply.github.com>
2022-03-07 07:56:03 -0800

    Update matrix.cpp
    Update the header comment

--- matrix.cpp
+++ matrix.cpp
@@ -1,19 +1,19 @@
-/*===============================================================
+/*=============================================================== 
 CSUF, CPSC254 - SPRING 2022
 Contributor: Trong Pham, Adam Harb, Albert Paez
 Project Title: Tictactoe Game
 Project Description: Standalone game allows user to beat AI.
-										 The winner is defined that has a cross
-										 of row, column, or diagonal. This game
-										 is designed with a smart AI to challenge
-										 players like you!
-										 This project includes main files of matrix
-										 operators, tictactoe game driver, and main
-										 file to call driver and load the game.
+  The winner is defined that has a cross
+  of row, column, or diagonal. This game
+  is designed with a smart AI to challenge
+  players like you!
+  This project includes main files of matrix
+  operators, tictactoe game driver, and main
+  file to call driver and load the game.
 License: Included but not limited of MIT, Harvard, CSUF, Github
 File Name: const.cpp
 File Description: Define all matrix operators being declared in
-									matrix.h
+  matrix.h
 ================================================================*/
 #pragma once
 #ifndef __MATRIX_CPP__
