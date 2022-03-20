2daca78544c4565a5044a9adadf10d2c92b61b9a
Adam Harb <adamharb1999@csu.fullerton.edu>
2022-03-20 11:27:25 -0700

    Implemented main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*=============================================================== 
+/*===============================================================
 CSUF, CPSC254 - SPRING 2022
 Contributor: Trong Pham, Adam Harb, Albert Paez
 Project Title: Tictactoe Game
@@ -18,12 +18,33 @@ File Description: Calling game driver file to load the game.
 #ifndef __MAIN_CPP__
 #define __MAIN_CPP__
 
-#include "tictactoe.h"
+#include "tictactoe.h" //header file
 
-int main() 
+int main()
 {
-	//TODO
-	return 0;
+	tictactoe game; //initializing variable tictactoe to game
+	cout<<"--------------------------"<<endl;
+	cout<<"\t\t TIC-TAC-TOE"<<endl; //write for display
+	cout<<"--------------------------"<<endl;
+	char cont='y';
+	do //do-while loop
+	{
+		char choice; //making a choice
+	 	cout << "Do you want to start first?(y/n):" <<endl; //Choose yes or no
+	 	cin >> choice; //user can can enter what they desire
+
+		if(choice=='n') //if they choose no
+			game.play(AI); //the opponent will start first
+		else if(choice=='y') //if they choose yes
+			game.play(PLAYER);//the player will start first
+		else //if they pick something other than yes or no
+			cout << "Invalid choice" << endl; //not the right choice...try again
+
+		cout <<"Do you want to quit(y/n):" << endl; //Do you want to give up?
+		cin >> cont; //yes or no
+	}
+	while(cont=='n'); //they don't want to play the game
+	return 0; //return the function
 }
 
 #endif
