83fe8ce55429979767dede64071ab12deb64389d
Trong D. Pham <70780829+phdtrong@users.noreply.github.com>
2022-05-13 19:55:49 -0700

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -7,46 +7,45 @@ Project Description: Standalone game allows user to beat AI.
   of row, column, or diagonal. This game
   is designed with a smart AI to challenge
   players like you!
-  This project includes main files of Matrix
+  This project includes main files of matrix
   operators, tictactoe game driver, and main
   file to call driver and load the game.
 License: Included but not limited of MIT, Harvard, CSUF, Github
-File Name: Matrix.cpp
-File Description: Define all Matrix operators being declared in Matrix.h
+File Name: matrix.cpp
+File Description: Define all matrix operators being declared in matrix.h
 ================================================================*/
 #pragma once
-#ifndef __Matrix_CPP__
-#define __Matrix_CPP__
+#ifndef __MATRIX_CPP__
+#define __MATRIX_CPP__
 
 #include "matrix.h"
-template <class T> T& Matrix<T>::operator()(const int row, const int column) // get reference of value
-{
+// get reference of a matrix element at (row, column) position
+template <class T> T& Matrix<T>::operator()(const int row, const int column) {
   return mat_[row][column];
 }
-template <class T> T Matrix<T>::operator()(const int row, const int column) const // get value
-{
+// get value of a matrix element at (row, column) position
+template <class T> T Matrix<T>::operator()(const int row, const int column) const {
   return mat_[row][column];
 }
-template <class T> T& Matrix<T>::operator()(const int array_index) // get reference of value
-{
+// get reference of a matrix element at (one_side_array index)
+template <class T> T& Matrix<T>::operator()(const int array_index) {
   return mat_[array_index/SIDE][array_index%SIDE];
 }
-template <class T> T Matrix<T>::operator()(const int array_index) const // get value
-{
+// get value of a matrix element at (one_side_array index)
+template <class T> T Matrix<T>::operator()(const int array_index) const {
   return mat_[array_index/SIDE][array_index%SIDE];
 }
-template <class T> void Matrix<T>::operator=(const T value) // assign the whole Matrix by 1 const value
-{
+// assign the whole matrix by 1 const value
+template <class T> void Matrix<T>::operator=(const T value) {
   for (int i = 0; i < SIDE; i++)
     for (int j = 0; j < SIDE; j++)
       (*this)(i, j) = value;
 }
-template <class T> bool Matrix<T>::Exist_Equalized_Row() const // a row with same value
-{
+// check to see if there is any row with same value
+template <class T> bool Matrix<T>::Exist_Equalized_Row() const {
   Matrix m = *this;
 	int same_value_cases=0;
-  for (int i = 0; i < SIDE; i++)
-	{
+  for (int i = 0; i < SIDE; i++) {
 		same_value_cases = 0;
 		for (int j = 0; j < SIDE; j++)//how many same value pairs
     	if (m(i, 0) == m(i, j))//on the same row
@@ -56,12 +55,11 @@ template <class T> bool Matrix<T>::Exist_Equalized_Row() const // a row with sam
 	}
   return false;
 }
-template <class T> bool Matrix<T>::Exist_Equalized_Column() const // a column with same value
-{
+// check to see if there is any column with same value
+template <class T> bool Matrix<T>::Exist_Equalized_Column() const {
 	Matrix m = *this;
 	int same_value_cases=0;
-  for (int j = 0; j < SIDE; j++)
-	{
+  for (int j = 0; j < SIDE; j++) {
 		same_value_cases = 0;
 		for (int i = 0; i < SIDE; i++)//how many same value pairs
     	if (m(0, j) == m(i, j))//on the same column
@@ -71,8 +69,8 @@ template <class T> bool Matrix<T>::Exist_Equalized_Column() const // a column wi
 	}
   return false;
 }
-template <class T> bool Matrix<T>::Exist_Equalized_Main_Diagonal() const // a main diagonal with same value
-{
+// check to see if there is a main diagonal with same value
+template <class T> bool Matrix<T>::Exist_Equalized_Main_Diagonal() const {
   Matrix m = *this;
 	int same_value_cases=0;
   for (int i = 0; i < SIDE; i++)
@@ -83,8 +81,8 @@ template <class T> bool Matrix<T>::Exist_Equalized_Main_Diagonal() const // a ma
 			return true;
   return false;
 }
-template <class T> bool Matrix<T>::Exist_Equalized_Sub_Diagonal() const // a sub diagonal with same value
-{
+// check to see if there is a sub diagonal with same value
+template <class T> bool Matrix<T>::Exist_Equalized_Sub_Diagonal() const {
 	Matrix m = *this;
 	int same_value_cases=0;
   for (int i = 0; i < SIDE; i++)
@@ -95,8 +93,8 @@ template <class T> bool Matrix<T>::Exist_Equalized_Sub_Diagonal() const // a sub
 			return true;
   return false;
 }
-template <class T> bool Matrix<T>::Exist_Equalized_Diagonal() const // a diagonal with same value
-{
+// check to see if there is any diagonal with same value
+template <class T> bool Matrix<T>::Exist_Equalized_Diagonal() const {
   return Exist_Equalized_Main_Diagonal() || Exist_Equalized_Sub_Diagonal();
 }
 
